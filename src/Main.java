66b8429eb442d190fb931d7fa4f842ced4164152
Xafix18 <zari10mouad@gmail.com>
2025-03-08 18:21:02 +0100

    revert: Removed the String array

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,12 @@ import java.util.Scanner;
 
     public class Main {
         public static void main(String[] args) {
-            Scanner userInput = new Scanner((System.in));
-            System.out.println("Add task");
 
-            String[] task = new String[3];
-            task[0] = userInput.next();
-            task[1] = userInput.next();
-            task[2] = userInput.next();
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("Add task:");
 
-            System.out.println("Your To-do task is :" + task[0]);
-            System.out.println("Your To-do task is :" + task[1]);
-            System.out.println("Your To-do task is :" + task[2]);
+            String task = userInput.nextLine(); // Read the entire line as a single task
 
-
+            System.out.println("Your To-do task is: " + task);
         }
     }
